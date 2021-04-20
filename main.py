0e5eb5e72348f72de9080586a33a8c31758cfc4e
HyperSmoit <descultu.cristian99@gmail.com>
2021-04-20 12:38:20 +0300

    Added Balloons, Shurikens

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import random
 import pygame, sys, os
 from pygame.locals import *
 import argparse
+import math
 
 pygame.init()
 frame_rate = pygame.time.Clock()
@@ -12,38 +13,99 @@ WIDTH = 1280
 HEIGHT = 720
 PLAYER_DIMENSIONS = (WIDTH // 10, HEIGHT // 4)
 PLAYER_VELOCITY = 10
-
+SHURIKEN_DIMENSIONS = (WIDTH // 30, HEIGHT // 10)
+SHURIKEN_SPEED = 7
+BALLOON_SPEED = 2
+BALLOON_DIMENSIONS = (WIDTH // 20, HEIGHT // 7)
 
 class Shuriken(pygame.sprite.Sprite):
-    pass
+    def __init__(self, pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = None
+        self.rect = Rect(0, 0, 0, 0)
+        self.pos = pos
+    
+    def draw(self):
+        self.image = pygame.transform.scale(pygame.image.load("shuriken.png"), SHURIKEN_DIMENSIONS)
+        self.image.set_colorkey((255, 255, 255))
+        self.rect = self.image.get_rect()
 
-class Balloons(pygame.sprite.Sprite):
-    pass
+    def update(self):
+        self.pos[0] -= SHURIKEN_SPEED
+        self.rect.move_ip(self.pos[0], 0)
+        
+    def collision(self, other):
+        pass
 
-class Player(pygame.sprite.Sprite):
+class Balloon(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = None
+        self.rect = Rect(0, 0, 0, 0)
+        self.pos = pos
+    
+    def draw(self):
+        self.image = pygame.transform.scale(pygame.image.load("balloon.png").convert(), BALLOON_DIMENSIONS)
+        self.image.set_colorkey((255, 255, 255))
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        self.pos[1] -= BALLOON_SPEED
+        self.rect.move_ip(0, self.pos[1])
+
+    def collision(self, other):
+        pass
+
+class Arrow(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = None
         self.rect = Rect(0, 0, 0, 0)
+        self.pos = []
+    
+    def draw(self):
+        pass
+
+    def update(self):
+        pass
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = None
+        self.rect = Rect(0, 0, 0, 0)
+        self.pos = pos
         self.y = 0
+        self.faceVector = [1,0]
 
     def draw(self):
-        self.image = pygame.transform.scale(pygame.image.load("archer.png"), PLAYER_DIMENSIONS)
+        self.image = pygame.transform.scale(pygame.image.load("archer.png").convert_alpha(), PLAYER_DIMENSIONS)
         self.image.set_colorkey((255, 255, 255))
         self.rect = self.image.get_rect() 
       
     def update(self):
+        self.rotate()
         self.rect.move_ip(0, self.y)
-        print(self.rect.top)
         if self.rect.top > HEIGHT:
             self.rect.top = HEIGHT
         if self.rect.bottom < 0:
             self.rect.bottom = 0
+        self.pos[1] = self.y
+        
     def moveUp(self):
         self.y += -PLAYER_VELOCITY
 
     def moveDown(self):
         self.y += PLAYER_VELOCITY
+    def rotate(self):
+        mouse_pos = pygame.mouse.get_pos()
+        rel_x, rel_y = mouse_pos[0], mouse_pos[1] - self.y
+        angle = -math.degrees(math.atan2(rel_y, rel_x))
+        # Rotate the image.
+        self.image = pygame.transform.rotozoom(self.image, angle, 1)
+        # Update the rect and keep the center at the old position.
+        self.rect = self.image.get_rect(center=self.rect.center)
+
 
 class Game:
 
@@ -51,14 +113,26 @@ class Game:
 
         self.window = pygame.display.set_mode((WIDTH, HEIGHT))
         self.gameObjects = []
-        self.player = Player()
+        self.player = Player([0,1])
         self.gameObjects.append(self.player)
+
+        for i in range (0, 20):
+            pos = [(random.uniform(2.6, 6.7)) * WIDTH / 2,random.randint(0, HEIGHT)]
+            self.shuriken = Shuriken(pos)
+            self.gameObjects.append(self.shuriken)
+
+        for i in range (0, 20):
+            pos = [random.uniform(WIDTH / 3, WIDTH), random.uniform(5 * HEIGHT, HEIGHT)]
+            self.balloon = Balloon(pos)
+            self.gameObjects.append(self.balloon)
+
 
     def run(self):
         while True:
+            self.draw()
             self.input()
             self.update()
-            self.draw()
+            
 
     def collisionDetection(self):
         pass
@@ -71,17 +145,27 @@ class Game:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-            elif event.type == pygame.KEYDOWN:
+            if event.type == pygame.KEYDOWN:
+                if event.key == K_q:
+                    pygame.quit()
                 if event.key == K_w:
                     self.player.moveUp()
                 if event.key == K_s:
                     self.player.moveDown()
+            # if event.type == MOUSEMOTION:
+            #     mouseX, mouseY = event.pos
+            #     rel_x, rel_y = mouseX, mouseY - self.player.y
+            #     angle = (180 / math.pi) * -math.atan2(rel_y, rel_x)
+            #     self.player.rotate(angle)
+                
+
+                
 
     def draw(self):
         self.window.fill(BACKGROUND_COLOR)
         for gameObject in self.gameObjects:
             gameObject.draw()
-        self.window.blit(self.player.image, [0,self.player.x])
+            self.window.blit(gameObject.image, gameObject.pos)
         pygame.display.update()
         frame_rate.tick(60)
 
